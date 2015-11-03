5ee64c9e8db7ee34fcfab54bccfbcbcbfb645d58
mookfist <mookfist@gmail.com>
2015-11-03 00:46:11 -0500

    new version of milights

--- script.service.mookfist-milights/service.py
+++ script.service.mookfist-milights/service.py
@@ -2,12 +2,28 @@ from light import Lights
 import xbmc
 import time
 
+class MyPlayer(xbmc.Player):
+  def __init__(self, lights):
+    xbmc.Player.__init__(self)
+    self.lights = lights
+  
+  def onPlayBackStarted(self):
+    l.fadeOff()    
+    xbmc.log('*********SOMETHING STARTED PLAYING', level=xbmc.LOGWARNING)
+
+  def onPlayBackStopped(self):
+    xbmc.log('*********SOMETHING STOPPED PLAYING', level=xbmc.LOGWARNING)
+    l.fadeOn()
+       
+        
+
 if __name__ == "__main__":
-  monitor = xbmc.Monitor()
 
   l = Lights('192.168.1.167')
   l.group = 1
-  l.initLights()
+
+  monitor = xbmc.Monitor()
+  player = MyPlayer(l)
 
   while not monitor.abortRequested():
     if monitor.waitForAbort(10):
