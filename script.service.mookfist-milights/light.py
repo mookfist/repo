a356566f4d7f5c21764a956a6236cd0e0fd32171
mookfist <mookfist@gmail.com>
2015-11-04 00:26:59 -0500

    More flexible version of Mookfist Milights!

--- script.service.mookfist-milights/light.py
+++ script.service.mookfist-milights/light.py
@@ -3,9 +3,6 @@ import time
 
 class Lights(object):
 
-  STATE_OFF = 1
-  STATE_ON  = 2
-
   def __init__(self, host, port=8899, bulbtype='rgb'):
     self.host = host
     self.port = 8899
@@ -16,54 +13,53 @@ class Lights(object):
     self.r = 255
     self.g = 80
     self.b = 0
-    
-    self.light_state = Lights.STATE_OFF
 
     self.controller = milight.MiLight({'host': self.host})
     self.light = milight.LightBulb(self.bulbtype)
 
-  def initLights(self):
-    for i in range(1,3):
-      self.controller.send(self.light.on(self.group))
-      time.sleep(0.1)
+    self._brightness = None
+    self._interrupt = False
 
-    for i in range(1,3):
-      self.controller.send(self.light.brightness(100, self.group))
-      time.sleep(0.1)
 
-    for i in range(1,3):
-      self.controller.send(self.light.color(milight.color_from_rgb(self.r,self.g,self.b), self.group))
-      time.sleep(0.1)
+  def interrupt(self):
+    self._interrupt = True
 
-    for i in range(1,3):
-      self.controller.send(self.light.brightness(1, self.group))
-      time.sleep(0.1)
 
-    for i in range(1,3):
-      self.controller.send(self.light.off(self.group))
-      time.sleep(0.1)
-
-    self.light_state = Lights.STATE_OFF
-    
   def fadeOn(self):
-    for i in range(1,100):
-      self.controller.send(self.light.brightness(i, self.group))
+    if self._brightness == None:
+      minRange = 1
+    else:
+      minRange = self._brightness
 
-    self.light_state = Lights.STATE_ON
+    for i in range(minRange,100):
+      if self._interrupt == False:
+        self._brightness = i
+        self.controller.send(self.light.brightness(i, self.group))
+      else:
+        break
+
+    self._interrupt = False
+
 
   def fadeOff(self):
-    for i in range(100,1,-1):
-      self.controller.send(self.light.brightness(i,self.group))
-   
-    self.light_state = Lights.STATE_OFF
-   
-  def isOn(self):
-    if self.light_state == Lights.STATE_OFF:
-      return False
-    return True
+    if self._brightness == None:
+      minRange = 100
+    else:
+      minRange = self._brightness
 
-  def isOff(self):
-    if self.light_state == Lights.STATE_ON:
-      return False
-    return True
+    for i in range(minRange,1,-1):
+      if self._interrupt == False:
+        self._brightness = i
+        self.controller.send(self.light.brightness(i,self.group))
+      else:
+        break
+    self._interrupt = False   
 
+  def setBrightness(self, brightness):
+    self.controller.send(self.light.brightness(brightness, self.group))
+    self._brightness = brightness
+
+  def setColor(self, r, g, b):
+    self.controller.send(self.light.color(milight.color_from_rgb(r,g,b), self.group))
+
+   
--- script.service.mookfist-milights/service.py
+++ script.service.mookfist-milights/service.py
@@ -1,6 +1,7 @@
 from light import Lights
 import xbmc, xbmcaddon
 import time
+import simplejson as json
 
 __scriptname__ = "Mookfist Milights"
 __author__     = "Mookfist"
@@ -9,27 +10,124 @@ __settings__   = xbmcaddon.Addon(id='script.service.mookfist-milights')
 __version__    = __settings__.getAddonInfo('version')
 __language__   = __settings__.getLocalizedString
 
-
+'''
 class MyPlayer(xbmc.Player):
   def __init__(self, lights):
     xbmc.Player.__init__(self)
     self.lights = lights
-  
+
+  def onPlayBackPaused(self):
+    time.sleep(5)
+    self.lights.fadeOn()
+
   def onPlayBackStarted(self):
-    self.lights.fadeOff()    
+    if self.isPlayingVideo():
+      tag = self.getVideoInfoTag()
+      print "------------ TAG INFO ----------------"
+      print tag.getFirstAired()
+      print tag.getPremiered()
+      print "--------------------------------------"
+      self.lights.fadeOff()    
 
   def onPlayBackStopped(self):
     self.lights.fadeOn()
-       
-        
+'''    
+
+
+class MyMonitor(xbmc.Monitor):
+
+  def __init__(self, lights):
+    xbmc.Monitor.__init__(self)
+    self.lights = lights
+
+  
+  def log(self, msg, lvl=xbmc.LOGNOTICE):
+    msg = "[milights] - [%s] %s" % (lvl, msg)
+    xbmc.log(msg, level=lvl)
+
+
+  def _getPlayerType(self, data):
+    if data['item']['type'] == 'episode':
+      return 'tv'
+    elif data['item']['type'] == 'movie':
+      return 'movie'
+    else:
+      return None
+
+
+  def _onPlay(self, data):
+    playerType = self._getPlayerType(data)
+    self.lights.group = int(__settings__.getSetting('light_group'))
+
+    if playerType == None:
+      self.log('Could not determine player type: %s' % data, xbmc.LOGWARNING)
+      return
+
+    tvEnabled = __settings__.getSetting('tv_enabled')
+    movieEnabled = __settings__.getSetting('movie_enabled')
+
+    self.log('TV Enabled: %s -- Movie Enabled: %s -- Player Type: %s' % (tvEnabled, movieEnabled, playerType))
+
+    if playerType == 'tv' and tvEnabled == 'true':
+      self.log('TV show has started playing, time to fade out the lights')
+      self.lights.fadeOff()
+    elif playerType == 'movie' and movieEnabled == 'true':
+      self.log('Movie has started playing, time to fade out the lights')
+      self.lights.fadeOff()
+
+
+  def _onStop(self, data):
+    playerType = self._getPlayerType(data)
+    self.lights.group = int(__settings__.getSetting('light_group'))
+
+    if playerType == None:
+      self.log('Coult not determine player type: %s' % data, xbmc.LOGWARNING)
+      return
+
+    if playerType == 'tv' and __settings__.getSetting('tv_enabled') == 'true':
+      self.log('TV show has stopped playing, time to fade on the lights')
+      self.lights.fadeOn()
+    elif playerType == 'movie' and __settings__.getSetting('movie_enabled') == 'true':
+      self.log('Movie show has stopped playing, time to fade on the lights')
+      self.lights.fadeOn()
+
+
+  def onNotification(self, sender, method, data):
+
+    self.log("SENDER: %s --- METHOD %s --- DATA %s" % (sender, method, data))
+
+    data = json.loads(data)
+
+    if str(sender) == "xbmc" and str(method) == "Player.OnPlay":
+      self.log("XBMC Player starts")
+      self._onPlay(data)
+    elif str(sender) == "xbmc" and str(method) == "Player.OnStop":
+      self.log("XBMC Player ends")
+      self._onStop(data)
+      
 
 if __name__ == "__main__":
 
   l = Lights(__settings__.getSetting('light_host'))
   l.group = int(__settings__.getSetting('light_group'))
 
-  monitor = xbmc.Monitor()
-  player = MyPlayer(lights=l)
+  monitor = MyMonitor(lights=l)
+
+  xbmc.log('-- LIGHT GROUP: %s' % l.group)
+
+  if __settings__.getSetting('reset_brightness') == 'true':
+    brightness = int(float(__settings__.getSetting('start_brightness')))
+    xbmc.log('-- INIT BRIGHTNESS WITH %s' % brightness)
+    l.setBrightness(brightness)
+
+  if __settings__.getSetting('enable_color_correction') == 'true':
+    r = int(float(__settings__.getSetting('red_value')))
+    g = int(float(__settings__.getSetting('green_value')))
+    b = int(float(__settings__.getSetting('blue_value')))
+    
+    xbmc.log('-- INIT COLOR WITH (%s, %s, %s)' % (r, g, b))
+
+    l.setColor(r,g,b)
 
   while not monitor.abortRequested():
     if monitor.waitForAbort(10):
