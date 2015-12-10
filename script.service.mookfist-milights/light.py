ec1629b1a3dd4720323e93fce8f7cab59b8abe29
mookfist <mookfist@gmail.com>
2015-12-10 12:11:29 -0500

    More refactoring Custom Milight bulb in order to make setting bulb types easier Normalized some argument orders This fixes issue #16 and issue #15

--- script.service.mookfist-milights/light.py
+++ script.service.mookfist-milights/light.py
@@ -1,11 +1,28 @@
 import milight
 import time
 import threading
-from utils import *
+from utils import log
+from importlib import import_module
 
 SPEED_SLOW = 1
 SPEED_MEDIUM = 5
 SPEED_FAST = 10
+
+class MilightBulb(milight.LightBulb):
+
+  def __init__(self, types=('rgbw','white','rgb')):
+    self.setTypes(types)
+
+  def setTypes(self, types=('rgbw','white','rgb')):
+    if type(types) is str:
+      types = (types,)
+
+    self._types = []
+    for t in types:
+      try:
+        self._types.append(import_module('.' + t, 'milight'))
+      except ImportError:
+        print('Unsupported bulb type: %s' % t)
 
 class LightFadeThread(threading.Thread):
   '''Managed fade queues
@@ -14,9 +31,9 @@ class LightFadeThread(threading.Thread):
   This thread manages a queue of brightness adjustment commands
   to get from the current brightness to a target globalMinBrightness
   '''
-  def __init__(self, light):
+  def __init__(self, lights):
     threading.Thread.__init__(self)
-    self.light = light
+    self.lights = lights
     self.running = False
 
     self._queue = []
@@ -36,7 +53,7 @@ class LightFadeThread(threading.Thread):
 
 
   def fade(self, brightness, step=1, group=1):
-    startingBrightness = self.light.brightness(group)
+    startingBrightness = self.lights.brightness(group)
 
     groupQueue = []
     for i in range(startingBrightness, brightness + step, step):
@@ -58,14 +75,13 @@ class LightFadeThread(threading.Thread):
     while self.running == True:
       if len(self._queue) > 0:
         task = self._queue.pop(0)
-        self.light.brightness(task[0], task[1])
+        self.lights.brightness(task[0], task[1])
+      else:
+        time.sleep(0.01)
 
 
 class Light(object):
-  '''Represents a Light
-
-  Technically, this represents a group.
-  '''
+  '''Represents a Light Group'''
   def __init__(self, milight, group=1, maxBrightness=100, minBrightness=0, color=(255,255,255)):
     self.maxBrightness = maxBrightness
     self.minBrightness = minBrightness
@@ -80,16 +96,19 @@ class Light(object):
     self._on = False
 
   def on(self):
+    '''turn on lights'''
     self._lightbulb.on(self.group)
     self._on = True
 
   def brightness(self, brightness=None):
+    '''Get and set brightness'''
     if brightness != None:
       self._controller.send(self._lightbulb.brightness(brightness, self.group))
       self._currentBrightness = brightness
     return self._currentBrightness
 
   def color(self, color=None):
+    '''Get and set color'''
     if color != None:
       rgb = milight.color_from_rgb(color[0], color[1], color[2])
       self._controller.send(self._lightbulb.color(rgb, self.group))
@@ -98,8 +117,15 @@ class Light(object):
 
 
 class Lights(object):
-
+  '''API to control multiple light groups'''
   def __init__(self, host, port=8899, bulbtype='rgbw', wait_duration=25):
+    '''Constructor
+
+    host - Wifi bridge host or IP address
+    port - Wifi bridge port number
+    bulbtype - Can be rgbw, rgb, or white
+    wait_duration - Interval between sending commands in milliseconds
+    '''
     self._host = host
     self._port = port
     self._bulbtype = bulbtype
@@ -107,55 +133,97 @@ class Lights(object):
     self._currentWorkingThread = None
 
     self._controller = milight.MiLight({'host': self._host, 'port': self._port}, wait_duration=wait_duration / 1000.0)
-    self._light = milight.LightBulb(self._bulbtype)
+    self._light = MilightBulb(self._bulbtype)
 
     self._lights = [None, None, None, None] # four groups maximum
 
 
   def setGroupLight(self, group, maxBrightness=100, minBrightness=1, color=(255,255,255)):
+    '''Setup lights for a group
+
+    group - Group number for the lights
+    maxBrightness - Maximum brightness of the lights (0 to 100)
+    minBrightness - Minimum brightness of the lights (100 to 0)
+    color - Tuple of red, green, and blue colors, eg (255,255,255) for white
+    '''
     self._lights[group-1] = Light((self._controller, self._light), group, maxBrightness, minBrightness, color)
 
   def removeGroupLight(self, group):
+    '''Remove lights for a group'''
     self._lights[group-1] = None
 
   def setHost(self, host, port=8899):
-    self.host = host
-    self.port = int(port)
-    self.controller._hosts = ({'host': host, 'port': port})
+    '''Set the host and port of the wifi bridge'''
+    self._host = host
+    self._port = port
+    self._controller._hosts = ({'host': host, 'port': port},)
 
   def setWaitDuration(self, wait_duration):
-    self._controller._wait = max(wait_duration / 1000, 0)
+    '''Set the interval in between commands in milliseconds'''
+    self._controller._wait = max(wait_duration / 1000.0, 0)
+
+  def setBulbType(self, bulbtype):
+    self._light.setTypes(bulbtype)
 
   def startFadeThread(self):
-    self._currentWorkingThread = LightFadeThread(light=self)
+    '''Start the worker thread that handles fading'''
+    self._currentWorkingThread = LightFadeThread(lights=self)
     self._currentWorkingThread.start()
 
   def stopFadeThread(self):
+    '''Stop the worker thread that handles fading'''
     if self._currentWorkingThread != None:
       self._currentWorkingThread.stop()
       self._currentWorkingThread = None
 
-  def brightness(self, group, brightness):
-    if self._lights[group-1] != None:
-      return self._lights[group-1].brightness(brightness)
+  def brightness(self, group, brightness=None):
+    '''Set a group to a specific brightness level'''
+    if len(self._lights) >= group and self._lights[group-1] != None:
+      if brightness != None:
+        log('Setting brightness for group #%s to %s%%' % (group, brightness))
+        return self._lights[group-1].brightness(brightness)
+      else:
+        return self._lights[group-1].brightness()
 
   def off(self, group):
+    '''Turn off a group'''
     if self._lights[group-1] != None:
       self._lights[group-1].off()
 
   def on(self, group):
+    '''Turn on a group'''
     if self._lights[group-1] != None:
       self._lights[group-1].on()
 
   def fade(self, group, target, step=1):
-    if self._lights[group-1] != None:
+    '''Fade a group to a target brightness
+
+    group - group number
+    target - target brightness (0 to 100)
+    step - Speed (1 is slowest)
+    '''
+
+    if len(self._lights) >= group and self._lights[group-1] != None:
+
+      l = self._lights[group-1]
+
+      if l.brightness() != None and l.brightness() > target:
+        step = -step
+
+      log('Fading lights for group #%s to %s%% brightness at a rate of %s' % (group, target, step))
       if self._currentWorkingThread == None:
         self.startFadeThread()
-
       self._currentWorkingThread.stopDimming(group)
       self._currentWorkingThread.fade(target, step, group)
 
+
   def color(self, group, rgb=None):
+    '''Set color of group
+
+    group - Group number
+    rgb - Tuple of red, green, blue values eg: (255,255,255) for white
+    '''
     if self._lights[group-1] != None:
+      log('Setting colors for group #%s to (%s,%s,%s)' % (group, rgb[0], rgb[1], rgb[2]))
       return self._lights[group-1].color(rgb)
 
--- script.service.mookfist-milights/service.py
+++ script.service.mookfist-milights/service.py
@@ -19,10 +19,6 @@ class MyMonitor(xbmc.Monitor):
     xbmc.Monitor.__init__(self)
     self.lights = None
 
-  def log(self, msg, lvl=xbmc.LOGNOTICE):
-    msg = "[milights] - [%s] %s" % (lvl, msg)
-    xbmc.log(msg, level=lvl)
-
   def _getPlayerType(self, data):
     if data['item']['type'] == 'episode':
       return 'tv'
@@ -36,78 +32,71 @@ class MyMonitor(xbmc.Monitor):
     playerType = self._getPlayerType(data)
 
     if playerType == None:
-      self.log('Could not determine player type: %s' % data, xbmc.LOGWARNING)
+      log('Could not determine player type: %s' % data)
       return
 
     tvEnabled = __settings__.getSetting('tv_enabled')
     movieEnabled = __settings__.getSetting('movie_enabled')
 
-    self.log('TV Enabled: %s -- Movie Enabled: %s -- Player Type: %s' % (tvEnabled, movieEnabled, playerType))
-
     if (playerType == 'tv' and tvEnabled == 'true') or (playerType == 'movie' and movieEnabled == 'true'):
-      self.log('-- Video has started playing')
+
       for i in range(1,4):
-        if groupEnabled(i):
-          stepSpeed = getStepSpeed(i)
-          if stepSpeed == None:
-            self.lights.brightness(getMinBrightness(i))
-          else:
-            self.lights.fade(getMinBrightness(i), stepSpeed, i)
+        stepSpeed = getStepSpeed(i)
+        if stepSpeed == None or self.lights.brightness(i) == None:
+          self.lights.brightness(i, getMinBrightness(i))
+        else:
+          self.lights.fade(i, getMinBrightness(i), stepSpeed)
 
 
   def _onStop(self, data):
     playerType = self._getPlayerType(data)
 
     if playerType == None:
-      self.log('Coult not determine player type: %s' % data, xbmc.LOGWARNING)
+      log('Coult not determine player type: %s' % data, xbmc.LOGWARNING)
       return
 
     if (playerType == 'tv' and __settings__.getSetting('tv_enabled') == 'true') or (playerType == 'movie' and __settings__.getSetting('movie_enabled') == 'true'):
 
       for i in range(1,4):
-        if groupEnabled(i):
-          stepSpeed = getStepSpeed(i)
-          if stepSpeed == None:
-            self.lights.brightness(100, i)
-          else:
-            self.lights.fade(getMaxBrightness(i), stepSpeed, i)
+        stepSpeed = getStepSpeed(i)
+        if stepSpeed == None or self.lights.brightness(i) == None:
+          self.lights.brightness(i, getMaxBrightness(i))
+        else:
+          self.lights.fade(i, getMaxBrightness(i), stepSpeed)
 
 
   def onSettingsChanged(self):
-    if self.lights != None:
-      self.lights.stopFadeThread()
 
-    self.log(__settings__.getSetting('port'))
     host = __settings__.getSetting('light_host')
     port = __settings__.getSetting('light_port')
-
-    if port == None:
-      port = 8899
-    else:
-      try:
-        port = int(port)
-      except ValueError:
-        port = 8899
-
     # @TODO This needs to change so different groups can have different bulb types
     bulbtype = getBulbType(1)
     wait_duration = int(__settings__.getSetting('command_delay'))
 
-    self.lights = Lights(host, port, bulbtype, wait_duration)
+    try:
+      port = int(port)
+    except ValueError:
+      port = 8899
+
+    if self.lights == None:
+      self.lights = Lights(host, port, bulbtype, wait_duration)
+    else:
+      self.lights.stopFadeThread()
+      self.lights.setHost(host, port)
+      self.lights.setBulbType(bulbtype)
+      self.lights.setWaitDuration(wait_duration)
+
 
     for i in [0,1,2,3]:
       group = i + 1
 
       if groupEnabled(group):
-        self.log('setGroupLight()')
         self.lights.setGroupLight(group, maxBrightness=getMaxBrightness(group), minBrightness=getMinBrightness(group), color=getRgbColor(group))
 
         if doInitBrightness(group):
-          self.log('doInitBrightness - %s - %s' % (getMaxBrightness(group), group))
           self.lights.brightness(group, getMaxBrightness(group))
 
         if doInitColor(group):
-          self.log('doInitColor')
           self.lights.color(group, getRgbColor(group))
       else:
         self.lights.removeGroupLight(group)
@@ -115,15 +104,13 @@ class MyMonitor(xbmc.Monitor):
 
   def onNotification(self, sender, method, data):
 
-    self.log("SENDER: %s --- METHOD %s --- DATA %s" % (sender, method, data))
+    # log("SENDER: %s --- METHOD %s --- DATA %s" % (sender, method, data))
 
     data = json.loads(data)
 
     if str(sender) == "xbmc" and str(method) == "Player.OnPlay":
-      self.log("XBMC Player starts")
       self._onPlay(data)
     elif str(sender) == "xbmc" and str(method) == "Player.OnStop":
-      self.log("XBMC Player ends")
       self._onStop(data)
 
 
